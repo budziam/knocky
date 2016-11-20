d107c8fe64e622a2eb05707c59297e8213f45b4a
Michał Budziak <michal.mariusz.b@gmail.com>
2016-11-20 23:04:02 +0100

    Refactor code

--- knocky.ino
+++ knocky.ino
@@ -1,83 +1,102 @@
 #include <Wire.h>
 
-byte DEVICE_ADDRESS = 0x53; //This address is fixed for the board we use because pin 12 is fixed to GND
+// This address is fixed for the board we use because pin 12 is fixed to GND
+byte DEVICE_ADDRESS = 0x53;
 
-byte DATA_FORMAT = 0x31;   //Data Format & Measurement Range Register
-byte POWER_CTRL = 0x2D;    //Power Control Register
-byte INT_ENABLE = 0x2E;    //Enable Data Ready Interrupt
+byte DATA_FORMAT = 0x31;
+byte POWER_CTRL = 0x2D;
+byte INT_ENABLE = 0x2E;
 
-byte DATAX0 = 0x32;        //X-Axis Data 0
-
-byte values[6];
+// Beginning of accelerate values data block as they are stored on 6 bytes
+byte ACCELERATION_DATA = 0x32;
 
 void setup()
 {
-  Wire.begin();    //Initiate the Wire library and join the I2C bus as a master. This is called only once.
-  Serial.begin(9600);
+    // Initiate the Wire library and join the I2C bus as a master.
+    Wire.begin();
+    Serial.begin(9600);
 
-  //Minimum initialization sequance according to the application note.
-  writeRegister(DEVICE_ADDRESS, DATA_FORMAT, 0x0B); //Put the ADXL345 into Measurement Mode with full resolution (13-bit) and +/-16G range.
-  writeRegister(DEVICE_ADDRESS, POWER_CTRL, 0x08); //Start Measurement.
-  writeRegister(DEVICE_ADDRESS, INT_ENABLE, 0x80); //Enable Data Ready Interrupt.
+    // Minimum initialization sequance according to the application note.
+    // Put the ADXL345 into Measurement Mode
+    // with full resolution (13-bit) and +/-16G range.
+    writeRegister(DEVICE_ADDRESS, DATA_FORMAT, 0x0B);
+    
+    // Start measurement
+    writeRegister(DEVICE_ADDRESS, POWER_CTRL, 0x08);
+
+    // Enable data ready interrupt
+    writeRegister(DEVICE_ADDRESS, INT_ENABLE, 0x80);
 }
 
 void loop()
 {
-    int x, y, z;
-    normalMode(x, y, z);
+    printAxis();
+}
 
+void printAxis()
+{
+    int x, y, z;
+    
+    getAxesValues(x, y, z);
+    
     Serial.print(x, DEC);
-    Serial.print(", ");
+    Serial.print("\t\t");
     Serial.print(y, DEC);
-    Serial.print(", ");
+    Serial.print("\t\t");
     Serial.print(z, DEC);
     Serial.println();
 }
 
-void normalMode(int &x, int &y, int &z)
+/**
+ * Get acceleration values for each axis
+ */
+void getAxesValues(int &x, int &y, int &z)
 {
-  readRegister(DEVICE_ADDRESS, DATAX0, 6, values); 
+    byte values[6];
+    readRegister(DEVICE_ADDRESS, ACCELERATION_DATA, 6, values); 
 
-  x = getAccelerationValue(values[0], values[1]);
-  y = getAccelerationValue(values[2], values[3]);
-  z = getAccelerationValue(values[4], values[5]);
-  
-  delay(15);
+    x = calculateAxisValue(values[0], values[1]);
+    y = calculateAxisValue(values[2], values[3]);
+    z = calculateAxisValue(values[4], values[5]);
+    
+    delay(15);
 }
 
 /**
- * The ADXL345 gives 10-bit or 13-bit acceleration values, but they are stored as bytes (8-bits).
+ * The ADXL345 gives 10-bit or 13-bit acceleration values, but they are stored
+ * as bytes (8-bits).
  * To get the full value, two bytes must be combined for each axis.
  */
-int getAccelerationValue(int youngerByte, int olderByte)
+int calculateAxisValue(int youngerByte, int olderByte)
 {
 	return olderByte << 8 | youngerByte;
 }
 
 void writeRegister(byte device, byte registerAddress, byte value)
 {
-  Wire.beginTransmission(device);    //Start transmission to device
-  Wire.write(registerAddress);       //Specify the address of the register to be written to
-  Wire.write(value);                 //Send the value to be writen to the register
-  Wire.endTransmission();            //End transmission 
+    Wire.beginTransmission(device);
+    
+    // Specify the address of the register to be written to
+    Wire.write(registerAddress);       
+    
+    // Send the value to be writen to the register
+    Wire.write(value);
+    
+    Wire.endTransmission(); 
 }
 
 void readRegister(byte device, byte registerAddress, int numBytes, byte *values)
 {
-  byte address = registerAddress;
+    Wire.beginTransmission(device);
+    Wire.write(registerAddress);
+    Wire.endTransmission();
 
-  Wire.beginTransmission(device);
-  Wire.write(address);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(device);     
-  Wire.requestFrom(device, numBytes);  //Request 6 bytes from device
-
-  int i = 0;
-  while (Wire.available() && i < numBytes) {        //Device may send less than requested
-    values[i] = Wire.read();                        //Receive a byte from device and put it into the buffer
-    i++;
-  }
+    Wire.beginTransmission(device); 
+    Wire.requestFrom(device, numBytes); // Request numBytes bytes from device
+    
+    for(int i = 0; Wire.available() && i < numBytes; ++i) {
+        values[i] = Wire.read()
+    }
   
-  Wire.endTransmission(); 
+    Wire.endTransmission(); 
 }
